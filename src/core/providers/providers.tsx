ebc95bb27b0e726f0406fad7f8f97117b9ca641d
Ulugbek <ulugb3k270@gmail.com>
2026-05-13 10:05:26 +0500

    feat: integrate SolarProvider in Providers component and replace ShoppingBag with Bag4 in PromoBanner

--- src/core/providers/providers.tsx
+++ src/core/providers/providers.tsx
@@ -3,13 +3,14 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { getQueryClient } from "@/core/providers/get-query-client";
+import { SolarProvider } from "@solar-icons/react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const queryClient = getQueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
-      {children}
+      <SolarProvider>{children}</SolarProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
--- src/features/home/components/promo-banner.tsx
+++ src/features/home/components/promo-banner.tsx
@@ -1,4 +1,4 @@
-import { ShoppingBag } from "lucide-react";
+import { Bag4 } from "@solar-icons/react/ssr";
 
 interface IProps {
   label: string;
@@ -8,16 +8,18 @@ interface IProps {
 
 export function PromoBanner({ label, text, cta }: IProps) {
   return (
-    <div className="mx-auto max-w-360 px-6 py-6">
-      <div className="flex items-center justify-center gap-4 rounded-[18px] bg-foreground px-8 py-2.5 text-background">
-        <ShoppingBag className="size-4" />
-        <span className="text-sm font-bold uppercase tracking-wider">
-          {label}
-        </span>
+    <div className="mx-auto max-w-360 px-6 py-6 ">
+      <div className="flex items-center justify-center gap-4 cursor-pointer rounded-[18px] bg-foreground px-8 py-2.5 text-background">
+        <div className="flex items-center gap-1.5">
+          <Bag4 className="size-4.5" weight="Bold" />
+          <span className="text-sm font-bold uppercase tracking-wider">
+            {label}
+          </span>
+        </div>
         <span className="text-sm">{text}</span>
         <button
           type="button"
-          className="text-sm font-semibold text-amber-300 hover:text-amber-200"
+          className="text-sm font-bold text-[#ECBB0D] transition-colors"
         >
           {cta}
         </button>
