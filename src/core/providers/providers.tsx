953a25f89384990e449bb7f292050b66d30ddc2c
Ulugbek <ulugb3k270@gmail.com>
2026-05-21 15:23:54 +0500

    Remove QueryClient setup and provider from core providers
    - Deleted the get-query-client.ts file which contained the QueryClient setup and related logic.
    - Updated providers.tsx to remove the QueryClientProvider and its dependencies, simplifying the provider structure.

--- src/core/providers/providers.tsx
+++ src/core/providers/providers.tsx
@@ -1,19 +1,13 @@
 "use client";
 
-import { QueryClientProvider } from "@tanstack/react-query";
-// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { getQueryClient } from "@/core/providers/get-query-client";
 import { SolarProvider } from "@solar-icons/react";
 import { Toaster } from "@/shared/components/ui/sonner";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const queryClient = getQueryClient();
-
   return (
-    <QueryClientProvider client={queryClient}>
+    <>
       <SolarProvider>{children}</SolarProvider>
       <Toaster />
-      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
-    </QueryClientProvider>
+    </>
   );
 }
